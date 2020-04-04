7bcbb6cdcfd278e61da852ff03925cf8f0defbb6
w-beom <60081555+w-beom@users.noreply.github.com>
2020-04-04 17:45:28 +0900

    4일차 문제

---
+++ src/leetCode30Day/Day4.java
@@ -0,0 +1,25 @@
+package leetCode30Day;
+
+public class Day4 {
+	public static void main(String[] args) {
+		int[] nums = { 4, 2, 4, 0, 0, 3, 0, 5, 1, 0 };
+		solution(nums);
+		for (int i : nums) {
+			System.out.println(i);
+		}
+	}
+
+	public static int[] solution(int[] nums) {
+		int temp = 0;
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i+1; j < nums.length; j++) {
+				if (nums[i] == 0) {
+					temp = nums[i];
+					nums[i] = nums[j];
+					nums[j] = temp;
+				}
+			}
+		}
+		return nums;
+	}
+}
